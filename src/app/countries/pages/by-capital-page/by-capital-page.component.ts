9b1817f1306e648e9cf07b7bb03b00b49cf0f311
Carlota-Cordero <carlota.cordero@deister.es>
2024-12-04 11:02:35 +0100

    Functionality Search

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
-  selector: 'app-by-capital-page',
+  selector: 'countries-by-capital-page',
   templateUrl: './by-capital-page.component.html',
   styles: ``
 })
 export class ByCapitalPageComponent {
 
+  public countries: Country[] = [];
+
+  constructor( private CountriesService: CountriesService ) { }
+
   searchByCapital(term: string): void {
-    console.log(term);
+    this.CountriesService.searchCapital(term)
+      .subscribe( countries => {
+        this.countries = countries
+      });
   }
 
 }
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Country } from '../interfaces/country';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+export class CountriesService {
+
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor( private http: HttpClient) { }
+
+  searchCapital( term: string ): Observable<Country[]> {
+
+    const url = `${ this.apiUrl }/capital/${ term }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) )
+      );
+
+  }
+
+  searchCountry( term: string ): Observable<Country[]> {
+
+    const url = `${ this.apiUrl }/name/${ term }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) )
+      );
+
+  }
+
+  searchRegion( region: string ): Observable<Country[]> {
+
+    const url = `${ this.apiUrl }/region/${ region }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) )
+      );
+
+  }
+
+}
